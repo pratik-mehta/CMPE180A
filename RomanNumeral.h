445fc25befd68c8718bdebd076cb3e7bce93938b
Pratik Mehta <pratikmehta1624@gmail.com>
2018-02-28 23:34:24 -0800

    Update RomanNumeral.h

--- RomanNumeral.h
+++ RomanNumeral.h
@@ -1,1 +1,39 @@
+#ifndef ROMANNUMERAL_H_
+#define ROMANNUMERAL_H_
 
+using namespace std;
+
+class RomanNumeral
+{
+public:
+    RomanNumeral();              //default constructor
+    RomanNumeral(int decimal);   //constructor with integer parameter
+    RomanNumeral(string roman);  //constructor with string parameter
+    ~RomanNumeral();             //deconstructor
+    
+    int get_decimal() const;  //public getter for decimal value
+    string get_roman() const; //public getter for roman numeral
+    
+    //Overload the arithmetic operators  + - * /
+    RomanNumeral operator +(const RomanNumeral& num);
+    RomanNumeral operator -(const RomanNumeral& num);
+    RomanNumeral operator *(const RomanNumeral& num);
+    RomanNumeral operator /(const RomanNumeral& num);
+
+    //Overload the equality operators  == !=
+    bool operator ==(const RomanNumeral& num);
+    bool operator !=(const RomanNumeral& num);
+
+    //Overload the stream operators  >>  and  <<
+    friend istream& operator >>(istream& in, RomanNumeral& num);
+    friend ostream& operator <<(ostream& out, const RomanNumeral& num);
+
+private:
+   int decimal;   //decimal value as an integer
+   string roman;  //roman numeral as a string
+   
+   int to_decimal();    //convert roman numeral to decimal value
+   string to_roman();   //convert decimal value to roman numeral
+};
+
+#endif /* ROMANNUMERAL_H_ */
