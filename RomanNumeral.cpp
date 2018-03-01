646ff565c4955145d3c48143a98ed8ef2f0af232
Pratik Mehta <pratikmehta1624@gmail.com>
2018-02-28 23:34:53 -0800

    Update RomanNumeral.cpp

--- RomanNumeral.cpp
+++ RomanNumeral.cpp
@@ -1,1 +1,31 @@
+using namespace std;
+#include <cstdlib>
+#include <iostream>
+#include "RomanNumeral.h"
+
+
+RomanNumeral::RomanNumeral() : roman(""), decimal(0)
+{
+ //default constructor
+}
+
+RomanNumeral::RomanNumeral(int value) : decimal(value)
+{
+ //constructor with integer parameter
+}
+
+RomanNumeral::RomanNumeral(string str) : roman(str)
+{
+ //constructor with string parameter
+}
+
+RomanNumeral::~RomanNumeral()
+{
+ //deconstructor
+}
+
+int RomanNumeral::get_decimal() const {return decimal; }
+
+string RomanNumeral::get_roman() const {return roman; }
+
 
